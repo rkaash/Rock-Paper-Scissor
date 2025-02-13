53601d258d64493e24fec620ef05ceb2b7cab720
Rooben Prakaash Soomu <108934644+rkaash@users.noreply.github.com>
2025-02-13 22:30:11 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+// ROCK PAPER SCISSORS
+
+const choices = ["Rock" , "Paper" , "Scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0 ;
+let computerScore = 0 ;
+
+
+
+function playGame(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "" ; 
+
+    if(playerChoice == computerChoice){
+        result = "It's a tie !"
+    }
+
+    else{
+        switch(playerChoice){
+            case "Rock" :
+                result=(computerChoice === "Scissors") ? "YOU WIN !" : "YOU LOSE !";
+                break;
+            
+            case "Paper" :
+                result=(computerChoice === "Rock") ? "YOU WIN !" : "YOU LOSE !";
+                break;
+
+            case "Scissors" :
+                result=(computerChoice === "Paper") ? "YOU WIN !" : "YOU LOSE !";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `Player : ${playerChoice}`;
+    computerDisplay.textContent = `Computer : ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("greenText" , "redText" );
+
+        switch(result){
+            case "YOU WIN !":
+                resultDisplay.classList.add("greenText");
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+                break;
+
+            case "YOU LOSE !":
+                resultDisplay.classList.add("redText");
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+                break;
+
+        }
+}
+
+
